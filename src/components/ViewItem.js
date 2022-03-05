5b17500fa6a8315fb32898ad6832ae94b07708b2
Michael Jed Alamares <michael.alamares@paymaya.com>
2022-03-05 12:06:05 +0800

    added absolute path, loader, and refactor UserPageView

---
+++ src/components/ViewItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  textValue: {},
+  label: {
+    fontSize: 12,
+    fontWeight: 600,
+    color: "#757575",
+    marginBottom: 0,
+  },
+  value: {
+    fontSize: 16,
+  },
+}));
+
+const ViewItem = (props) => {
+  const classes = useStyles();
+  const { label, children } = props;
+  return (
+    <div className={classes.textValue}>
+      <div className={classes.label}>{label}</div>
+      <div className={classes.value}>{children}</div>
+    </div>
+  );
+};
+
+export default ViewItem;
--- src/pages/users/view/UserPage.js
+++ src/pages/users/view/UserPage.js
@@ -4,22 +4,13 @@ import { useUser } from "../../../hooks/users";
 import { makeStyles } from "@mui/styles";
 import { get } from "lodash";
 import Grid from "@mui/material/Grid";
-import ScopedCssBaseline from "@mui/material/ScopedCssBaseline";
+import Loader from "components/Loader";
+import ViewItem from "components/ViewItem";
 
 const useStyles = makeStyles((theme) => ({
   title: {
     fontSize: 30,
     fontWeight: 500,
-  },
-  textValue: {},
-  label: {
-    fontSize: 12,
-    fontWeight: 600,
-    color: "#757575",
-    marginBottom: 0,
-  },
-  value: {
-    fontSize: 16,
   },
 }));
 
@@ -29,80 +20,66 @@ const UsersPage = () => {
   const userQuery = useUser(id);
 
   const user = get(userQuery, "data", []);
-  console.log("userQuery", userQuery);
+  const isLoading = get(userQuery, "isLoading");
+
+  const {
+    firstName,
+    lastName,
+    mobileNumber,
+    address,
+    department,
+    position,
+    type,
+    initialPassword,
+  } = user;
+
+  if (isLoading) return <Loader />;
+
   return (
     <div>
-      <ScopedCssBaseline>
-        <div
-          style={{ display: "flex", alignItems: "center", marginBottom: 30 }}
+      <div style={{ display: "flex", alignItems: "center", marginBottom: 30 }}>
+        <h2 className={classes.title}>User Details</h2>
+        <span
+          style={{
+            marginLeft: 15,
+            background: type === "admin" ? "#4caf50" : "#42a5f5",
+            padding: "3px 7px",
+            fontSize: 12,
+            fontWeight: 600,
+            color: "#fff",
+            borderRadius: 15,
+          }}
         >
-          <h2 className={classes.title}>User Details</h2>
-          <span
-            style={{
-              marginLeft: 15,
-              background: user.type === "admin" ? "#4caf50" : "#42a5f5",
-              padding: "3px 7px",
-              fontSize: 12,
-              fontWeight: 600,
-              color: "#fff",
-              borderRadius: 15,
-            }}
-          >
-            {user.type}
-          </span>
-        </div>
+          {type}
+        </span>
+      </div>
 
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={4} lg={3}>
-            <div className={classes.textValue}>
-              <div className={classes.label}>First Name:</div>
-              <div className={classes.value}>{user.firstName}</div>
-            </div>
-          </Grid>
-          <Grid item xs={12} md={4} lg={3}>
-            <div className={classes.textValue}>
-              <div className={classes.label}>Last Name:</div>
-              <div className={classes.value}>{user.lastName}</div>
-            </div>
-          </Grid>
-          <Grid item xs={12} md={4} lg={3}>
-            <div className={classes.textValue}>
-              <div className={classes.label}>Mobile Number:</div>
-              <div className={classes.value}>{user.mobileNumber}</div>
-            </div>
-          </Grid>
-          <Grid item xs={12} md={4} lg={3}>
-            <div className={classes.textValue}>
-              <div className={classes.label}>Address:</div>
-              <div className={classes.value}>{user.address}</div>
-            </div>
-          </Grid>
-          <Grid item xs={12} md={4} lg={3}>
-            <div className={classes.textValue}>
-              <div className={classes.label}>Department:</div>
-              <div className={classes.value}>{user.department}</div>
-            </div>
-          </Grid>
-          <Grid item xs={12} md={4} lg={3}>
-            <div className={classes.textValue}>
-              <div className={classes.label}>Position:</div>
-              <div className={classes.value}>{user.position}</div>
-            </div>
-          </Grid>
-          <Grid item xs={12} md={4} lg={3}>
-            <div className={classes.textValue}>
-              <div className={classes.label}>User Type:</div>
-              <div className={classes.value}>{user.type}</div>
-            </div>
-          </Grid>
-          <Grid item xs={12} md={4} lg={3}>
-            <div className={classes.textValue}>
-              <div className={classes.label}>Generated Password</div>
-              <div className={classes.value}>{user.initialPassword}</div>
-            </div>
-          </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={4} lg={3}>
+          <ViewItem label="First Name:">{firstName}</ViewItem>
         </Grid>
-      </ScopedCssBaseline>
+        <Grid item xs={12} md={4} lg={3}>
+          <ViewItem label="Last Name:">{lastName}</ViewItem>
+        </Grid>
+        <Grid item xs={12} md={4} lg={3}>
+          <ViewItem label="Mobile Number:">{mobileNumber}</ViewItem>
+        </Grid>
+        <Grid item xs={12} md={4} lg={3}>
+          <ViewItem label="Address:">{address}</ViewItem>
+        </Grid>
+        <Grid item xs={12} md={4} lg={3}>
+          <ViewItem label="Department:">{department}</ViewItem>
+        </Grid>
+        <Grid item xs={12} md={4} lg={3}>
+          <ViewItem label="Position:">{position}</ViewItem>
+        </Grid>
+        <Grid item xs={12} md={4} lg={3}>
+          <ViewItem label="User Type:">{type}</ViewItem>
+        </Grid>
+        <Grid item xs={12} md={4} lg={3}>
+          <ViewItem label="Generated Password:">{initialPassword}</ViewItem>
+        </Grid>
+      </Grid>
     </div>
   );
 };
